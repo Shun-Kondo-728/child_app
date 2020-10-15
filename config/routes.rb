f4ebdbf0c16f924930dc723ad564c79a666a9afd
Shun-Kondo-728 <shun728kon@gmail.com>
2020-10-15 10:42:02 +0900

    Usersコントローラー作成・ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # frozen_string_literal: true
   root 'static_pages#home'
   get :help, to: 'static_pages#help'
+  get :signup, to: 'users#new'
+  resources :users
 end
