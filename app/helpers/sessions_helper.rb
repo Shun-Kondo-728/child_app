e4297c0e5fa74f65430171c1fc709cca444c8928
Shun-Kondo-728 <shun728kon@gmail.com>
2020-10-18 12:36:21 +0900

    forgetメソッド・バグ修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -29,5 +30,12 @@ module SessionsHelper
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,11 @@ end
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   private
 
     def downcase_email
