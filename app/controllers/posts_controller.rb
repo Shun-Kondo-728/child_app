bcfae60904f778ec05e8cf4568edd1e85a793996
Shun-Kondo-728 <shun728kon@gmail.com>
2020-10-30 16:01:51 +0900

    correct_userメソッド追加及びテスト

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @post = Post.new
@@ -37,5 +38,11 @@ class PostsController < ApplicationController
 
     def post_params
       params.require(:post).permit(:title, :description, :recommended)
+    end
+
+    def correct_user
+        # 現在のユーザーが更新対象の料理を保有しているかどうか確認
+        @post = current_user.posts.find_by(id: params[:id])
+        redirect_to root_url if @post.nil?
     end
 end
--- spec/requests/posts_edit_spec.rb
+++ spec/requests/posts_edit_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "post edit", type: :request do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
   let!(:new_post) { create(:post, user: user) }
 
   context "for authorized users" do
@@ -30,6 +31,22 @@ RSpec.describe "post edit", type: :request do
                                                    recommended: 4 } }
       expect(response).to have_http_status "302"
       expect(response).to redirect_to login_path
+    end
+  end
+
+  context "for users of different accounts" do
+    it "redirect to home screen" do
+      # 編集
+      login_for_request(other_user)
+      get edit_post_path(new_post)
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
+      # 更新
+      patch post_path(new_post), params: { post: {  title: "赤ちゃんが泣き止む曲",
+                                                    description: "この曲が、一番オススメです！",
+                                                    recommended: 4 } }
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
     end
   end
 end
