7e6e6c0d47b39a6e6ce7369425356c01b719bbb2
Shun-Kondo-728 <shun728kon@gmail.com>
2020-10-30 14:57:17 +0900

    投稿更新処理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "投稿が更新されました！"
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def post_params
