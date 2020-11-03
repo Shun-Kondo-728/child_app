ddc5d8eaa1044b261858f863fa76b8e1168ce7ef
Shun-Kondo-728 <shun728kon@gmail.com>
2020-11-04 00:28:02 +0900

    受動的フォロー機能、テスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
                                   foreign_key: "follower_id",
                                   dependent: :destroy
   has_many :following, through: :active_relationships,  source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
   before_save :downcase_email
   validates :name, presence: true, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -61,6 +65,11 @@ class User < ApplicationRecord
   # 現在のユーザーがフォローしてたらtrueを返す
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  # 現在のユーザーがフォローされていたらtrueを返す
+  def followed_by?(other_user)
+    followers.include?(other_user)
   end
 
   private
