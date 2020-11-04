2de1aa213cd61de4108b8bc4ce92e7b797083555
Shun-Kondo-728 <shun728kon@gmail.com>
2020-11-04 19:50:55 +0900

    フォロー・フォロワー数表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get :help, to: 'static_pages#help'
   get :signup, to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :posts
   get    :login,     to: 'sessions#new'
   post   :login,     to: 'sessions#create'
