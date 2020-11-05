75801fadf1869e3efa3cf0ebb70ca791ec8ce7d4
Shun-Kondo-728 <shun728kon@gmail.com>
2020-11-05 12:57:03 +0900

    いいね機能のcreate・destroyアクション追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,21 @@ class LikesController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @post = Post.find(params[:post_id])
+    @user = @post.user
+    current_user.like(@post)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    current_user.likes.find_by(post_id: @post.id).destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
