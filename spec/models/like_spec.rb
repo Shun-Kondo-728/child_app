314719b8832a4d4f1370956c1c10b0f24cfbff30
Shun-Kondo-728 <shun728kon@gmail.com>
2020-11-05 12:11:27 +0900

    Likeモデルにバリデーション、テスト追加

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:like) { create(:like) }
+
+  it "the like instance is valid" do
+    expect(like).to be_valid
+  end
+
+  it "invalid if user_id is nil" do
+    like.user_id = nil
+    expect(like).not_to be_valid
+  end
+
+  it "Invalid if post_id is nil" do
+    like.post_id = nil
+    expect(like).not_to be_valid
+  end
 end
