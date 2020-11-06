b436a415ea2d5c0a755f37b2fef87b021c3cbe3f
Shun-Kondo-728 <shun728kon@gmail.com>
2020-11-06 16:24:59 +0900

    Rubocopエラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,6 @@ class User < ApplicationRecord
   attr_accessor :remember_token
 
   class << self
-
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
