ec74154e4596014c227a77a017d7e0e3d9e5a3d5
Shun-Kondo-728 <shun728kon@gmail.com>
2020-11-08 13:15:44 +0900

    Commentsモデル関連付け、バリデーション追加、テスト

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    post_id 1
-    user_id 1
-    content "MyText"
+    user_id { 1 }
+    content { "すごい参考になりました。ありがとうございます。" }
+    association :post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:comment) { create(:comment) }
+
+  context "validation" do
+    it "must be in a valid state" do
+      expect(comment).to be_valid
+    end
+
+    it "must be invalid without user_id" do
+      comment = build(:comment, user_id: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "if there is no post_id, it is in an invalid state" do
+      comment = build(:comment, post_id: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "if there is no content, it is in an invalid state" do
+      comment = build(:comment, content: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "the content must be 50 characters or less" do
+      comment = build(:comment, content: "あ" * 51)
+      comment.valid?
+      expect(comment.errors[:content]).to include("は50文字以内で入力してください")
+    end
+  end
 end
