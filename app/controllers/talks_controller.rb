f04b008ccb909f35e4bcd934f2482de8ab5a4286
Shun-Kondo-728 <shun728kon@gmail.com>
2020-11-11 11:15:13 +0900

    Talks,Messages,Membershipsコントローラメソッド追加、追記

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,5 +1,6 @@
 class TalksController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_member, only: [:show, :messages]
 
   def show
     @messages = @talk.messages
@@ -13,4 +14,32 @@ class TalksController < ApplicationController
     @talk.save
     redirect_to @talk
   end
+
+  def messages
+    @message = Message.new(message_params)
+    @talk.touch
+    if @message.save
+      redirect_to @talk
+    else
+      @messages = @talk.messages
+      render "show"
+    end
+  end
+
+  private
+
+    def message_params
+      params[:message].merge!({ user_id: current_user.id, talk_id: @talk.id })
+      params.require(:message).permit(:user_id, :talk_id, :content)
+    end
+
+    def correct_member
+      @talk = current_user.talks.find_by(id: params[:id])
+      redirect_to root_url if @talk.nil?
+    end
+
+    def correct_user
+      @message = current_user.messages.find_by(id: params[:id])
+      redirect_to root_url if @message.nil?
+    end
 end
