d4ad14e39e921d7b05886791eb95942606671a95
Shun-Kondo-728 <shun728kon@gmail.com>
2020-11-30 11:34:59 +0900

    Notificationsモデル修正・テスト

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :notification do
-    visiter_id 1
-    visited_id 1
-    post_id 1
-    comment_id 1
-    action "MyString"
+    visiter_id { 1 }
+    visited_id { 2 }
+    post_id { 3 }
+    comment_id nil
+    action { "like" }
     checked false
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:notification) { create(:notification) }
+
+  it "the notification instance is valid" do
+    expect(notification).to be_valid
+  end
+
+  it "if visiter_id is nil, it is invalid" do
+    notification.visiter_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "if visited_id is nil, it is invalid" do
+    notification.visited_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "if action is nil, it is invalid" do
+    notification.action = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "if checked is nil, it is invalid" do
+    notification.checked = nil
+    expect(notification).not_to be_valid
+  end
 end
