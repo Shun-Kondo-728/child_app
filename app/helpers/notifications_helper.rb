0367685c8ba78b05faee0f77132337b013fa3990
Shun-Kondo-728 <shun728kon@gmail.com>
2020-12-01 15:40:32 +0900

    通知一覧画面作成

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,18 @@
 module NotificationsHelper
+  def notification_form(notification)
+    @visiter = notification.visiter
+    @comment = nil
+    @visiter_comment = notification.comment_id
+    case notification.action
+    when 'follow'
+      tag.a(notification.visiter.name, href: user_path(@visiter)) + 'があなたをフォローしました'
+    when 'like'
+      tag.a(notification.visiter.name, href: user_path(@visiter)) + 'が' + tag.a('あなたの投稿', href: post_path(notification.post_id)) + 'にいいねしました'
+    when 'comment' then
+      @comment = Comment.find_by(id: @visiter_comment)
+      @comment_content =@comment.content
+      @post_title =@comment.post.title
+      tag.a(@visiter.name, href: user_path(@visiter)) + 'が' + tag.a("#{@post_title}", href: post_path(notification.post_id)) + 'にコメントしました'
+    end
+  end
 end
