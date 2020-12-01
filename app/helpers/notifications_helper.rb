550a6adf7134ecf4e382a32c4a92b96b755dba0d
Shun-Kondo-728 <shun728kon@gmail.com>
2020-12-02 00:31:31 +0900

    未確認通知があるときのマーク表示

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -15,4 +15,8 @@ module NotificationsHelper
       tag.a(@visiter.name, href: user_path(@visiter)) + 'が' + tag.a("#{@post_title}", href: post_path(notification.post_id)) + 'にコメントしました'
     end
   end
+
+  def unchecked_notifications
+    @notifications=current_user.passive_notifications.where(checked: false)
+  end
 end
