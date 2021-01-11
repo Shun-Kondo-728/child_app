202599c97db7f67e894c0e2538592349dc299dd4
Shun-Kondo-728 <shun728kon@gmail.com>
2021-01-11 13:56:25 +0900

    problem_commentsコントローラに通知作成メソッド呼び出し追加

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -16,18 +16,18 @@ class Problem < ApplicationRecord
           save_notification_problem_comment!(current_user, problem_comment_id, temp_id['user_id'])
         end
         save_notification_problem_comment!(current_user, problem_comment_id, user_id) if temp_ids.blank?
+    end
+
+    def save_notification_problem_comment!(current_user, problem_comment_id, visited_id)
+      notification = current_user.active_notifications.new(
+        problem_id: id,
+        problem_comment_id: problem_comment_id,
+        visited_id: visited_id,
+        action: 'problem_comment'
+      )
+      if notification.visiter_id == notification.visited_id
+        notification.checked = true
       end
-    
-      def save_notification_problem_comment!(current_user, problem_comment_id, visited_id)
-        notification = current_user.active_notifications.new(
-          problem_id: id,
-          problem_comment_id: problem_comment_id,
-          visited_id: visited_id,
-          action: 'problem_comment'
-        )
-        if notification.visiter_id == notification.visited_id
-          notification.checked = true
-        end
-        notification.save if notification.valid?
-      end
+      notification.save if notification.valid?
+    end
 end
