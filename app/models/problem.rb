d37a018974f170fdbf5f9018d2f138765a0eecf1
Shun-Kondo-728 <shun728kon@gmail.com>
2021-01-11 11:56:39 +0900

    Notificationモデルの関連付け追加

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,7 @@
 class Problem < ApplicationRecord
     belongs_to :user
     has_many :problem_comments, dependent: :destroy
+    has_many :notifications, dependent: :destroy
     default_scope -> { order(created_at: :desc) }
     validates :user_id, presence: true
     validates :description, presence: true, length: { maximum: 200 }
--- app/models/problem_comment.rb
+++ app/models/problem_comment.rb
@@ -1,5 +1,6 @@
 class ProblemComment < ApplicationRecord
-belongs_to :problem
+  belongs_to :problem
+  has_many :notifications, dependent: :destroy
   validates :user_id, presence: true
   validates :problem_id, presence: true
   validates :content, presence: true, length: { maximum: 100 }
