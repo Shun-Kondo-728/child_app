21e195e37c5debb7990b01663e8cc34239cc8ec7
Shun-Kondo-728 <shun728kon@gmail.com>
2021-01-20 12:20:01 +0900

    application_helper.rb修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   require "uri"
 
-  def text_url_to_link(text)
+  def text_url_to_link text
     URI.extract(text, ['http', 'https']).uniq.each do |url|
       sub_text = ""
       sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
